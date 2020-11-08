e1e094ce3d229fe9c1150c6b65f3bec2f7cd8b0a
Christina Lee <christina98lee@gmail.com>
2020-11-08 10:57:34 -0500

    Added navbar

--- ui/src/Components/SearchCandidates.jsx
+++ ui/src/Components/SearchCandidates.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Table, Button, Col, Form, FormGroup, Label, Input } from 'reactstrap';
+import NavBar from './NavBar';
 
 
 export default class SearchCandidates extends React.Component {
@@ -23,14 +24,19 @@ export default class SearchCandidates extends React.Component {
 	}
 
 	render(){
+		// Add results row by row
 		if (this.state.search) {
 			const obj = this.state.search.result;
 			var objLen = Object.keys(obj).length;
-			//console.log(obj);
-			//console.log(objLen);
+			console.log(obj);
+			console.log(objLen);
+
+			// const filt1 = ;
+			// const filt2 = ;
 
 			var items = [];
 			for (var i = 0; i < objLen; i++) {
+				// if (with all filter conditions)
 				items.push(<tr>
 								<th scope="row">{i}</th>
 								<td>{this.state.search.result[i].first_name} {this.state.search.result[i].last_name}</td>
@@ -47,9 +53,9 @@ export default class SearchCandidates extends React.Component {
 					{!this.state.search ? (
 						<div> No results retrieved from DB </div>
 					)  :  (
-					<>
 					<Form>
-						<h3> Search Candidates </h3>
+					<NavBar />
+						<h3> Search Cleaners </h3>
 						<FormGroup row>
 							<Label for="text-search" sm={2}>Name</Label>
 							<Col sm={10}>
@@ -69,14 +75,12 @@ export default class SearchCandidates extends React.Component {
 							</Col>
 						</FormGroup>
 						<FormGroup row>
-							<Col sm={{ size: 10, offset: 2 }}>
+							<Col sm={2}>
 							<Button color="primary onClick={this.applySearch}">Search</Button>
 							</Col>
 						</FormGroup>
 
-					</Form>
-
-					<div className="searchResults">
+						<div className="searchResults">
 						<h3>Results</h3>
 						<p>(Found {objLen} RESULTS)</p>
 						<Table>
@@ -93,7 +97,7 @@ export default class SearchCandidates extends React.Component {
 							  </tbody>
 							</Table>
 						</div>
-						</>
+					</Form>
 					)}
 				</>
 		);
