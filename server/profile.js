2fb7121537219044400c4477349d8549ddd06ec6
nicolasabousawan <nicolasabousawan@users.noreply.github.com>
2020-12-01 17:43:42 -0800

    added enum states and admin views
    - view Profile has an admin view now
    - GET API now updates app state (verified and pending)
    - PUT API now updates app state (between in progress and pending)
    - search routes to admin view

--- server/profile.js
+++ server/profile.js
@@ -1,16 +1,37 @@
 const { Pool, Client } = require('pg')
 
 const pool = new Pool({
-  user: process.env.user,
-  host: process.env.host,
-  database: process.env.database,
-  password: process.env.password,
-  port: process.env.port,
+    user: process.env.user,
+    host: process.env.host,
+    database: process.env.database,
+    password: process.env.password,
+    port: process.env.port,
 });
+
+// Application States
+const READY_FOR_VEFIFICATION = 'Application Pending Employer Validation';
+const VERIFIED = 'Application Completed Employer Validation';
 
 module.exports.getProfile = async (event) => {
 
   let profileId = Number(event.queryStringParameters.id);
+  let verifiedState = Number(event.queryStringParameters.verified);
+  console.log(verifiedState)
+
+
+  /**
+   * Update the application state based on the type of call
+   */
+  let stateUpdate = "";
+  const appStatusUpdateSQL = 'UPDATE cleaner_profile SET app_status = $1 WHERE profile_id = $2';
+  if(verifiedState == 0){
+    const readyToVerifyValues = [READY_FOR_VEFIFICATION, profileId]
+    stateUpdate = await pool.query(appStatusUpdateSQL, readyToVerifyValues);
+  } 
+  if(verifiedState == 1){
+    const inProgressValues = [VERIFIED, profileId]
+    stateUpdate = await pool.query(appStatusUpdateSQL, inProgressValues);
+  }
 
   const selectProfile = "SELECT * FROM cleaner_profile WHERE profile_id = $1";
   const values = [profileId]
