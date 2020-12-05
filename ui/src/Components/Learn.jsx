d4b866e617e90b3c942a540c153ab6ba0c4bfcd7
nicolasabousawan <nicolasabousawan@users.noreply.github.com>
2020-12-04 16:00:25 -0800

    Updated the tag CSS
    Should match design standards

--- ui/src/Components/Learn.jsx
+++ ui/src/Components/Learn.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table } from 'reactstrap';
+import { Table, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 export default class ProfilePage extends React.Component {
@@ -10,13 +10,13 @@ export default class ProfilePage extends React.Component {
     render(){
       return(
         <Table className="row-buffer" responsive>
-            <h3> Start Learning Here! </h3>
+            <Label for="name" sm={4}><h4 className="left-center" >Start Learning Here</h4></Label>
             <tbody>
             <tr>
             <iframe width="648" height="365" src="https://www.youtube.com/embed/TqHX3pTeRdg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </tr> 
             <tr>
-                Cleaning Tips 101
+              <Label for="name" sm={2}>Cleaning Tips 101</Label> 
             </tr> <br></br>
 
 
@@ -24,14 +24,14 @@ export default class ProfilePage extends React.Component {
             <iframe width="648" height="365" src="https://www.youtube.com/embed/2fFaPDe5uI8" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </tr>
             <tr>
-                How to Mop
+              <Label for="name" sm={2}>How to Mop</Label> 
             </tr> <br></br>
             
             <tr>
             <iframe width="648" height="486" src="https://www.youtube.com/embed/iApBJYyi67k" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </tr>
             <tr>
-                Window Cleaning like a Pro
+              <Label for="name" sm={4}>Window Cleaning like a Pro</Label> 
             </tr> <br></br>
             </tbody>
 
